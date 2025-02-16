ce86620481a7c2aeaafc499cff72406d1216e8cd
hossein <hosseinmeysami.25@gmail.com>
2025-02-16 18:44:09 +0330

    Configure Prettier for consistent code style and formatting

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  bracketSpacing: true,
+  arrowParens: "always",
+  printWidth: 120,
+  trailingComma: "all",
+  endOfLine: "auto",
+};
